281f54575fd4b1692165b73c9634435a2987c2fb
Chad Milburn <chad.r.milburn@gmail.com>
2016-03-05 15:44:33 -0500

    transport exercises

---
+++ transport.js
@@ -0,0 +1,1 @@
+require('seneca')().use('plugin').listen(process.argv[2], '127.0.0.1')
---
+++ transportclient.js
@@ -0,0 +1,7 @@
+require('seneca')()
+  .client({type: 'tcp'})
+  .act({role: 'math', cmd: 'sum', left: process.argv[2], right: process.argv[3]}, function(err, result) {
+    if (err) return console.error(err)
+    console.log(result)
+    this.close()
+  })
